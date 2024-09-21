c720744c5a336e2c6fb8c00ae912e5b590d2e22a
Igor Janos <ijanos84@gmail.com>
2024-09-21 15:57:57 +0200

    Devcontainer

---
+++ docker/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Fail on error and unset variables.
+set -eu -o pipefail
+
+CWD=$(readlink -e "$(dirname "$0")")
+cd "${CWD}/.." || exit $?
+source ./docker/common.sh
+
+DEVICE=$1
+echo "Using GPU devices: ${DEVICE}"
+
+DEVICE_NAME=`echo ${DEVICE} | tr "," "-"`
+
+export USER_NAME=$(whoami)
+echo "User: ${USER_NAME}"
+
+
+docker run \
+    -it --rm \
+    --name "stuFieldReg-${DEVICE_NAME}" \
+    --gpus all \
+    --privileged \
+    --shm-size 64g \
+    --device /dev/fuse \
+    -v "${HOME}/.netrc":/root/.netrc \
+    -v "${CWD}/..":/workspace/${PROJECT_NAME} \
+    -v "/mnt/scratch/${USER}/.datasets":/mnt/datasets \
+    -v "/mnt/scratch/${USER}/${PROJECT_NAME}":/workspace/${PROJECT_NAME}/.scratch \
+    -e CUDA_VISIBLE_DEVICES="${DEVICE}" \
+    ${IMAGE_TAG} 
